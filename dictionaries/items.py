15f629aed271f72f54ed8cca029f0dd77cb26cc8
okekolawolesunday009 <okekolawolesunday@gmail.com>
2026-01-11 08:09:50 +0100

    classes, dict example

---
+++ dictionaries/items.py
@@ -0,0 +1,3 @@
+file_counts = {"jpg":10, "txt":14, "csv":2, "py":23}
+for ext, amount in file_counts.items():
+  print("There are {} files with the .{} extension".format(amount, ext))
---
+++ exercise/dict/group.py
@@ -0,0 +1,14 @@
+def groups_per_user(group_dictionary):
+    user_groups = {}
+    # Go through group_dictionary
+    for group_type, users in group_dictionary.items():
+        # Now go through the users in the group
+        for user in users:
+            # Now add the group to the list of groups for this user
+            # creating the entry in the dictionary if necessary
+            if user not in user_groups:
+                user_groups[user] = []
+            user_groups[user].append(group_type)
+    return user_groups
+
+print(groups_per_user({"local": ["admin", "userA"], "public": ["admin", "userB"], "administrator": ["admin"]}))
